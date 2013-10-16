38cca88f94a6a70e666c47f062a38c0137369408
Brett Batie <brett@batie.com>
2013-10-16 09:29:01 -0700

    Auto Commit of Application List for Desktop Work

---
+++ bin/mountfs.sh
@@ -0,0 +1,7 @@
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" ift-tuk001:/ /home/brett/mount/ft-tuk001
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" iweb05:/ /home/brett/mount/web05
+#sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" ift-tuk001:/ /home/brett/mount/ft-tuk001
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" idev01:/ /home/brett/mount/dev01
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" batie:/ /home/brett/mount/batie
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" abatches:/ /home/brett/mount/abatches
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" adb01:/ /home/brett/mount/adb01
---
+++ bin/umountfs.sh
@@ -0,0 +1,2 @@
+fusermount -u /home/brett/mount/web05
+fusermount -u /home/brett/mount/ft-tuk001
