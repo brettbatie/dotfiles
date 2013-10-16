184d90493fdb9a2b1108a270828bdc11da28d99d
Brett Batie <brett@batie.com>
2013-10-16 09:49:49 -0700

    Added logic to determine which editor to set as the default

--- source/apps.sh
+++ source/apps.sh
@@ -1,4 +1,11 @@
-export EDITOR='subl -w'
+# Set the default editor based on what is installed on the system
+if hash subl >/dev/null 2>&1; then
+    export EDITOR='subl -w'
+elif hash vim >/dev/null 2>&1; then
+    export EDITOR='vim'
+elif hash vi >/dev/null 2>&1; then
+    export EDITOR='vi'
+fi
 export VISUAL=$EDITOR
 
 # Easier navigation: .., ..., -
