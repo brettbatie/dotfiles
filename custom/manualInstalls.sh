020f3d08d6d8a638de03749ed3fabfcba5b395a4
Brett Batie <brett@batie.com>
2013-10-16 09:31:44 -0700

    Adding some more manual steps to take on new systems

--- custom/manualInstalls.sh
+++ custom/manualInstalls.sh
@@ -1,2 +1,13 @@
 #Install IE Virtual Machines
 curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | bash
+#Install Glances Monitoring tool
+pip install Glances
+
+#Install quicktile
+# dependencies
+sudo apt-get install python python-gtk2 python-xlib python-dbus python-wnck
+mkdir -p /opt/local/quicktile
+cd /opt/quicktile
+git https://github.com/ssokolow/quicktile.git .
+chmod +x setup.py
+sudo ./setup.py install
