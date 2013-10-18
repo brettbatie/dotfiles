99e11e240a961f8ff6c0a57dbb134694b72eae46
Brett Batie <brett@batie.com>
2013-10-18 13:31:44 -0700

    added aliases

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -3,3 +3,16 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
+
+
+# IP info
+alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias myip="wanip"
+
+# List Directories
+alias lsd="ls -ld .*/"
+
+# -E Keep current environment when doing a sudo
+# Space at end allows alias expansion after sudo (ex sudo ll)
+
+alias sudo="sudo -E "
