8290ae8cf4a2aabe129334876b75d80fa1b6db3f
Brett Batie <brett@batie.com>
2013-10-18 13:31:56 -0700

    Added more manual install steps

--- custom/manualInstalls.sh
+++ custom/manualInstalls.sh
@@ -21,3 +21,23 @@ http://www.squirrelsql.org/#installation
 # Cron
 # Creates a list of applications installed on this computer and adds it to source control
 * */2 * * * cd $HOME/dotfiles && bin/saveApps desktop-work --quite && git add custom/desktop-work.list && git commit -m "Auto Commit of Application List for Desktop Work" && git push
+
+
+# Setup Display driver: 
+sudo aptitude install fglrx fglrx-control
+aticonfig --initial --adapter=all
+
+# Install crashplan:
+Download/Unzip
+sudo ./install
+#Note: I had to run the following command to get the crashplan service to automatically start after boot:
+sudo rm /etc/rc2.d/S99crashplan
+sudo update-rc.d crashplan defaults
+
+# In Crashplan restore:
+# /home
+# /opt/
+# /usr/local/bin
+# but ignore the directories in /home: .cache, .icedove, .PlayOnLinux, .steam, .VirtualBox, bin, Desktop, Templates, Public, .shotwell, .steampath, .steampid, Documents, Music, Pictures, Videos
+
+# NOTE: if restoreing to a temp folder, make sure to cp -af .* to get the hidden directories to merge with the existing ones
