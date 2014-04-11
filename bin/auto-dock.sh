82e9dd932c7cd0161b74092d8e382945fe6ae592
Brett Batie <brett@batie.com>
2014-04-11 09:48:08 -0700

    Scripts to setup monitors when docked/undocked & setup monitors when bootmanager loads

---
+++ bin/auto-dock.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#Note: this script runs by adding a rule to udev. I created a file /etc/udev/rules.d/80-switch-monitors.rules and added the following line to it:
+#DEVPATH=="/devices/pci0000:00/0000:00:02.0/drm/card0", ACTION=="change", RUN+="/home/brett/dotfiles/bin/auto-dock.sh"
+
+
+#work in progress: I should cleanup the below commands to only work with one monitor at a time. Check for HDMI2 if present enable only HDMI2, etc
+# http://unix.stackexchange.com/questions/4489/a-tool-for-automatically-applying-randr-configuration-when-external-display-is-p
+echo "running" >> /home/brett/log.txt
+output=$(cat /sys/class/drm/card0-HDMI-A-2/status)
+export DISPLAY=:0.0
+export XAUTHORITY=/home/brett/.Xauthority
+
+if [ $output = disconnected ]; then
+    /usr/bin/xrandr --output DP3 --off \
+                    --output DP2 --off \
+                    --output DP1 --off \
+                    --output HDMI3 --off \
+                    --output HDMI2 --off \
+                    --output HDMI1 --off \
+                    --output LVDS1 --primary --mode 1600x900 --pos 0x0 --rotate normal \
+                    --output VGA1 --off
+elif [ $output = connected ]; then
+    /usr/bin/xrandr --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+                    --output HDMI3 --mode 1920x1080 --pos 1920x0 \
+                    --output LVDS1 --off
+fi
--- bin/dock.sh
+++ bin/dock.sh
@@ -1,41 +1,65 @@
-# If an external monitor is connected, place it with xrandr
-# External output may be "VGA" or "VGA-0" or "DVI-0" or "TMDS-1"
-EXTERNAL_OUTPUT="HDMI2"
-INTERNAL_OUTPUT="LVDS1"
-# EXTERNAL_LOCATION may be one of: left, right, above, or below
-EXTERNAL_LOCATION="right"
+#!/bin/bash
 
-case "$EXTERNAL_LOCATION" in
-    left|LEFT)
-        EXTERNAL_LOCATION="--left-of $INTERNAL_OUTPUT"
-        ;;
-    right|RIGHT)
-        EXTERNAL_LOCATION="--right-of $INTERNAL_OUTPUT"
-        ;;
-    top|TOP|above|ABOVE)
-        EXTERNAL_LOCATION="--above $INTERNAL_OUTPUT"
-        ;;
-    bottom|BOTTOM|below|BELOW)
-        EXTERNAL_LOCATION="--below $INTERNAL_OUTPUT"
-        ;;
-    *)
-        EXTERNAL_LOCATION="--left-of $INTERNAL_OUTPUT"
-        ;;
-esac
-#sleep 10;
-while true; do
-export DISPLAY=:0
-/usr/bin/xrandr |/bin/grep $EXTERNAL_OUTPUT | /bin/grep " connected "
+#Note: this script runs on boot to help the login manager to use the appropriate monitors. 
+# I modified the file /etc/lightdm/lightdm.conf and added the following line to the [SeatDefaults] section:
+# display-setup-script = /home/brett/dotfiles/bin/dock.sh
+
+/usr/bin/xrandr |/bin/grep "HDMI2 connected"
 if [ $? -eq 0 ]; then
-    echo "main"
-#    xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto $EXTERNAL_LOCATION 2>&1 | logger -t $0
-    # Alternative command in case of trouble:
-    # (sleep 2; xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto $EXTERNAL_LOCATION) &
-    /usr/bin/xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
-else
-    echo "else"
-#    xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off 2>&1 | logger -t $0
-    /usr/bin/xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1600x900 --pos     0x0 --rotate normal --output VGA1 --off
+    # initial monitor setup at (two monitors and docked laptop) but it tends to fail to initialize both monitors. 
+    /usr/bin/xrandr --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+                    --output HDMI3 --mode 1920x1080 --pos 1920x0 \
+                    --output LVDS1 --off
+    # Hack: the above command tends to not initialize both monitors. To fix this we disabled the monitors and try again.
+    sleep 1; 
+    /usr/bin/xrandr --output DP3 --off \
+                    --output DP2 --off \
+                    --output DP1 --off \
+                    --output HDMI3 --off \
+                    --output HDMI2 --off \
+                    --output HDMI1 --off \
+                    --output LVDS1 --primary --mode 1600x900 --pos 0x0 --rotate normal \
+                    --output VGA1 --off;
+
+    /usr/bin/xrandr --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+                    --output HDMI3 --mode 1920x1080 --pos 1920x0 \
+                    --output LVDS1 --off;
 fi
-sleep 5;
-done
+
+
+#FIXME: http://unix.stackexchange.com/questions/4489/a-tool-for-automatically-applying-randr-configuration-when-external-display-is-p
+# Use a udev rule.
+#
+# NOTE: the below commands work but is cpu intensive.
+
+#
+# while true; do
+#     # Check for connected but not in use
+#     /usr/bin/xrandr |/bin/grep "HDMI2 connected (normal"
+#     connected=$?
+
+#     # Check for disconnected
+#     /usr/bin/xrandr |/bin/grep "HDMI2 disconnected"
+#     disconnected=$?
+    
+#     # if the 2nd monitor is connected but not setup
+#     if [ $connected == 0 ]; then
+        
+#         # Setup secondary monitor
+#         /usr/bin/xrandr --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+#                         --output HDMI3 --mode 1920x1080 --pos 1920x0 \
+#                         --output LVDS1 --off
+#     # if the 2nd monitor is disconnected
+#     elif [ $disconnected == 0 ]; then
+#         # Setup laptop monitor
+#         /usr/bin/xrandr --output DP3 --off \
+#                         --output DP2 --off \
+#                         --output DP1 --off \
+#                         --output HDMI3 --off \
+#                         --output HDMI2 --off \
+#                         --output HDMI1 --off \
+#                         --output LVDS1 --primary --mode 1600x900 --pos 0x0 --rotate normal \
+#                         --output VGA1 --off
+#     fi
+#     sleep 4;
+# done
