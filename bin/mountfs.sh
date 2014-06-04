0d16cdb309153388e04330839c6244f56bd73ae8
Brett Batie <brett@batie.com>
2014-06-04 12:47:24 -0700

    commented mounts no longer used.

--- bin/mountfs.sh
+++ bin/mountfs.sh
@@ -1,10 +1,11 @@
-sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" ift-tuk001:/ /home/brett/mount/ft-tuk001
-sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" iweb05:/ /home/brett/mount/web05
 #sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" ift-tuk001:/ /home/brett/mount/ft-tuk001
-sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" idev01:/ /home/brett/mount/dev01
-sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" batie:/ /home/brett/mount/batie
-sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" abatches:/ /home/brett/mount/abatches
-sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" adb01:/ /home/brett/mount/adb01
-if [ "Brett-PC" == "`hostname`" ]; then
-    sshfs work:/home/brett/ /home/brett/mount/work
-fi
+#sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" iweb05:/ /home/brett/mount/web05
+#sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" ift-tuk001:/ /home/brett/mount/ft-tuk001
+#sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" idev01:/ /home/brett/mount/dev01
+sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" batie:/ /mnt/batie
+google-drive-ocamlfuse /mnt/gdrive
+#sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" abatches:/ /home/brett/mount/abatches
+#sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" adb01:/ /home/brett/mount/adb01
+#if [ "Brett-PC" == "`hostname`" ]; then
+#    sshfs work:/home/brett/ /home/brett/mount/work
+#fi
