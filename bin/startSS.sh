f4e9db55427ef8d9028a9f8e277c366f556db412
Brett Batie <brett@batie.com>
2014-06-04 12:47:53 -0700

    Changes to script that starts play

--- bin/startSS.sh
+++ bin/startSS.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+sleep 15
 #Load Environment
-$HOME/.profile;
+source $HOME/.profile;
 
 # Startup Core Smartsheet Environemnt
-cd /home/brett/git/core/dev2/;
-/usr/bin/vagrant up
+screen -S virtualbox -d -m /bin/bash -c "source $HOME/.profile; cd /home/brett/git/core/dev2/; /usr/bin/vagrant up"
 
 # Startup Labs (using play)
-screen -S play-labs -d -m /bin/bash -c "$HOME/.profile; cd /home/brett/git/platform-labs/; /opt/play-2.2.1/play run"
+screen -S play-labs -d -m /bin/bash -c "source $HOME/.profile; cd /home/brett/git/platform-labs/; /opt/play-2.2.1/play run"
