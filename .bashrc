6ef8af901faa0289259cbf5453c8643258251362
Brett Batie <brett@batie.com>
2014-08-24 09:10:35 -0700

    various

--- .bashrc
+++ .bashrc
@@ -15,3 +15,13 @@ function src() {
 }
 
 src
+
+# add this configuration to ~/.bashrc
+export HH_CONFIG=hicolor         # get more colors
+shopt -s histappend              # append new history items to .bash_history
+export HISTCONTROL=ignorespace   # leading space hides commands from history
+export HISTFILESIZE=10000        # increase history file size (default is 500)
+export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
+export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"   # mem/file sync
+bind '"\C-r": "\C-a hh \C-j"'    # bind hh to Ctrl-r
+
--- bin/mountfs.sh
+++ bin/mountfs.sh
@@ -3,7 +3,7 @@
 #sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" ift-tuk001:/ /home/brett/mount/ft-tuk001
 #sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" idev01:/ /home/brett/mount/dev01
 sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" batie:/ /mnt/batie
-google-drive-ocamlfuse /mnt/gdrive
+#google-drive-ocamlfuse /mnt/gdrive
 #sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" abatches:/ /home/brett/mount/abatches
 #sshfs -o sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" adb01:/ /home/brett/mount/adb01
 #if [ "Brett-PC" == "`hostname`" ]; then
