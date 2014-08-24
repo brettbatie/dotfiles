7531e100f9cc8264b578c4a78eaf1a2816434c9c
Brett Batie <brett@batie.com>
2014-08-24 09:10:52 -0700

    dock file for home computer

---
+++ bin/home-dock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI2 --mode 1680x1050 --pos 1680x0 --rotate normal --output HDMI3 --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 3360x0 --rotate normal --output VGA1 --off
