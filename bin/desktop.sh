cc84de7e2f9cf0caa48ca22a84e9fb6c4d8a6ca8
Brett Batie <brett@batie.com>
2014-08-28 11:35:13 -0700

    work in progess: setting up multiple monitors

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 3840x180 --rotate normal --output VGA1 --off
+xrandr --output HDMI3 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output HDMI2 --mode 1920x1080 --left-of HDMI3 --rotate normal
+xrandr --output LVDS1 --pos 3840x312 --mode 1360x768 --rotate normal
--- bin/laptop.sh
+++ bin/laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1360x768 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output HDMI2 --off --output HDMI3 --off
