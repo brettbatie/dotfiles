27ad887981828e790acbc0363d965468e4339706
Brett Batie <brett@batie.com>
2015-06-28 16:30:01 -0700

    Auto Commit of Application List for Desktop Work

---
+++ bin/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --mode 1920x1200 --pos 1680x0 --rotate right --output HDMI1 --off --output LVDS1 --off --output VGA1 --mode 1680x1050 --pos 0x384 --rotate normal
