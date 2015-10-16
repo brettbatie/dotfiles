c7c72d6266ad3e2f5631eeb5747089fa74fbd5c9
Brett Batie <brett@batie.com>
2015-10-16 14:21:01 -0700

    Auto Commit of Application List for Desktop Work

--- bin/installScripts/spotify.sh
+++ bin/installScripts/spotify.sh
@@ -1,6 +1,6 @@
 # 1. Add this line to your list of repositories by
 #    editing your /etc/apt/sources.list
-sudo sh -c 'echo deb http://repository.spotify.com stable non-free > /etc/apt/sources.list.d/spotify.list'
+sudo sh -c 'echo deb http://repository.spotify.com testing non-free > /etc/apt/sources.list.d/spotify.list'
 
 # 2. If you want to verify the downloaded packages,
 #    you will need to add our public key
