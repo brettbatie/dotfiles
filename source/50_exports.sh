1e7bb3504a068630d9931f8dbd5f3cd1c4c96c86
Brett Batie <brett@batie.com>
2015-11-20 10:12:47 -0800

    update to shell history

--- source/50_exports.sh
+++ source/50_exports.sh
@@ -16,8 +16,14 @@ export VISUAL=$EDITOR
 
 # History  settings
 export HISTTIMEFORMAT="[%F %T] "
-export HISTSIZE=1000000
-export HISTFILESIZE=1000000
+export HH_CONFIG=hicolor         # get more colors
+shopt -s histappend              # append new history items to .bash_history
+export HISTCONTROL=ignorespace   # leading space hides commands from history
+export HISTFILESIZE=10000        # increase history file size (default is 500)
+export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
+export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"   # mem/file sync
+# if this is interactive shell, then bind hh to Ctrl-r (for Vi mode check doc)
+if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hh \C-j"'; fi
 
 export M3_HOME=/opt/apache-maven-3.2.3/
 export M3=$M3_HOME/bin
