1b8905939b6076ff30245a8f2fc0c4599d806a32
Brett Batie <brett@batie.com>
2016-06-02 11:03:45 -0700

    various

--- bin/startSS.sh
+++ bin/startSS.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sleep 15
+#sleep 15
 #Load Environment
 source $HOME/.profile;
 
-# Startup Core Smartsheet Environemnt
-#screen -S virtualbox -d -m /bin/bash -c "source $HOME/.profile; cd /home/brett/git/core/dev2/; /usr/bin/vagrant up"
+coreDir=/home/brett/git/team-core-dev/app-core
 
-# Startup Labs (using play)
-#screen -S play-labs -d -m /bin/bash -c "source $HOME/.profile; cd /home/brett/git/platform-labs/; /opt/play-2.2.1/play run"
+cd $coreDir;
+docker-compose up -d;
+docker-compose logs;
