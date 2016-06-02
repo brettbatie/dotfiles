db2917646b8ce9c256b6424b4e7160d9ff792320
Brett Batie <brett@batie.com>
2016-06-02 11:06:44 -0700

    various

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -13,3 +13,6 @@ fi
 #unlimited history
 export HISTFILESIZE=
 export HISTSIZE=
+export HISTTIMEFORMAT="[%F %T] "
+export HISTFILE=~/.bash_eternal_history
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
