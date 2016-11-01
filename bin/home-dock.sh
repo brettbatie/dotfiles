af7fde688a3f977793815c086db5cc75330c57e0
Brett Batie <brett.batie@smartsheet.com>
2016-11-01 12:08:27 -0700

    Cleanup

--- bin/home-dock.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP3 --off --output DP2 --mode 2560x1440 --pos 1680x0 --rotate right --output DP1 --off --output HDMI3 --mode 1680x1050 --pos 0x728 --rotate normal --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
