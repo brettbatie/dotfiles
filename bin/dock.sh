9cea810a203accbc8ece7c9b7e978e6ed81b410f
Brett Batie <brett.batie@smartsheet.com>
2016-11-01 12:08:55 -0700

    Simplified switching monitors

--- bin/dock.sh
+++ bin/dock.sh
@@ -5,16 +5,7 @@
 #sleep 5
 # su - brett -c "/opt/autorandr/./autorandr.py --change"
 # su - brett -c "/opt/autorandr/./autorandr.py --load work"
-/opt/autorandr/./autorandr.py --change
-/opt/autorandr/./autorandr.py --load work
-
-# #/usr/bin/xrandr |/bin/grep "HDMI3 connected"
-# output=$(cat /sys/class/drm/card0-DP-2/status)
-# if [ $output = connected ]; then
-#     xrandr --output LVDS1 --off
-#     sleep 2;
-#     #xrandr --output DP1 --off --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP3 --mode 2560x1440 --pos 2560x0 --rotate normal --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
-#     #--output DP2 --mode 2560x1440 --pos 0x0 --rotate normal 
-# #    xrandr --output DP3 --mode 2048x1152 --pos 2048x0 --rotate normal --output DP2 --mode 2048x1152 --pos 0x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
-#     /home/brett/dotfiles/bin/./desktop.sh
-# fi
+#/opt/autorandr/./autorandr.py --change
+/opt/autorandr/./autorandr.py --load work1; 
+sleep 4;
+/opt/autorandr/./autorandr.py --load work2
